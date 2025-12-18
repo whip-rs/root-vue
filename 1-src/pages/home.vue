39e2f331336adf10e0620d35cca7a65e67e31c10
Nenad W. Stojanović <wiperr@gmail.com>
2025-12-18 13:44:46 +0100

    minor update

--- 1-src/pages/home.vue
+++ 1-src/pages/home.vue
@@ -1,22 +1,22 @@
 <template lang="pug">
-    .hero.flex.container
-        h1 Root Vue Template Repo
-        p home.vue
-        Button(@click="handleClick") Click the Button
-        p Click count: {{ count }}
+	.hero.flex.container
+		h1 Root Vue Template Repo
+		p home.vue
+		Button(@click="handleClick") Click the Button
+		p Click count: {{ count }}
 
-    </template>
+	</template>
 
 <style scoped>
-    .hero {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        min-height: 100dvh;
-        text-align: center;
-        gap: 1rem;
-        }
+	.hero {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		min-height: 100dvh;
+		text-align: center;
+		gap: 1rem;
+		}
 
 </style>
 
@@ -24,8 +24,8 @@
 import { ref } from 'vue'
 const count = ref(0)
 function handleClick() {
-    count.value++
-    }
+	count.value++
+	}
 // Touch the values in the script so TypeScript's `noUnusedLocals` doesn't error when Pug template usage is not picked up by the TS checker.
 void count.value
 void handleClick
